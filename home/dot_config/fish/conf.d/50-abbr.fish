cde6d3cfdf958fc2d38568e45749f290e5b152ea
horserr <112157036+horserr@users.noreply.github.com>
2026-05-06 00:50:51 +0800

    fish: add abbr

--- home/dot_config/fish/conf.d/50-abbr.fish
+++ home/dot_config/fish/conf.d/50-abbr.fish
@@ -1,7 +1,10 @@
 if status is-interactive
     abbr -a p printf
     # link: https://linuxize.com/post/how-to-find-files-in-linux-using-the-command-line/
-    abbr -a f find
+    abbr -a f "find . -maxdepth 1"
+    abbr -a find-broken "find . -xtype l"
+    # symbolic link dir
+    abbr -a lnd "ln -sfd"
 
     if command -q tldr
         abbr -a m tldr
@@ -18,3 +21,12 @@ if status is-interactive
     # abbr -a ;/ '\\'
     # abbr -a ;l '|'
 end
+
+# 软连接替换原子操作，避免服务中断
+# link: https://linuxize.com/post/how-to-create-symbolic-links-in-linux-using-the-ln-command/#overwriting-and-updating-symlinks
+# 第一步：先创建一个临时的快捷方式
+# ln -s /opt/app/releases/2.1.0 /opt/app/current.tmp
+
+# 第二步：瞬间替换成正式的快捷方式
+# mv -Tf /opt/app/current.tmp /opt/app/current
+
