b76a0b45e3a65368399dec572fd97ead53617357
horserr <112157036+horserr@users.noreply.github.com>
2026-05-06 13:56:59 +0800

    use Windows Browser inside WSL

--- home/dot_config/fish/conf.d/10-env.fish
+++ home/dot_config/fish/conf.d/10-env.fish
@@ -4,6 +4,12 @@ set -gx VISUAL vim
 set -gx RUSTUP_DIST_SERVER https://rsproxy.cn
 set -gx RUSTUP_UPDATE_ROOT https://rsproxy.cn/rustup
 
+# use windows browser tamer to browser webpage inside wsl
+if status is-interactive; and string match -qi "*microsoft*" </proc/sys/kernel/osrelease
+    set -gx BROWSER bt.exe
+end
+
+# homebrew
 if test -x /home/linuxbrew/.linuxbrew/bin/brew
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
@@ -12,6 +18,7 @@ end
 string match -q "$TERM_PROGRAM" vscode
 and . (code --locate-shell-integration-path fish)
 
+# forgit
 if test -d "$FORGIT_INSTALL_DIR/bin"
     fish_add_path "$FORGIT_INSTALL_DIR/bin"
 end
