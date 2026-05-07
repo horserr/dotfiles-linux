cb9e3c8773e52e2cff048c7b5b4afc0bce781578
horserr <112157036+horserr@users.noreply.github.com>
2026-05-07 17:52:51 +0800

    tmux: add basic config with fish alias

--- home/dot_config/fish/conf.d/50-abbr.fish
+++ home/dot_config/fish/conf.d/50-abbr.fish
@@ -1,5 +1,7 @@
 if status is-interactive
     abbr -a g git
+    abbr -a c cat
+
     abbr -a p printf
     # link: https://linuxize.com/post/how-to-find-files-in-linux-using-the-command-line/
     abbr -a f "find . -maxdepth 1"
@@ -7,14 +9,27 @@ if status is-interactive
     # symbolic link dir
     abbr -a lnd "ln -sfd"
 
+    if command -q tmux
+        abbr -a t tmux new-session
+        abbr -a tt tmux new-session -s
+        abbr -a ta tmux attach
+        abbr -a tls tmux list-sessions
+        abbr -a tk tmux kill-session -t
+        abbr -a tkill tmux kill-server
+        abbr -a tmv tmux move-window -s -t
+        abbr -a tlsk tmux list-keys -N
+    end
+
     if command -q tldr
         abbr -a m tldr
     end
+
     if command -q uv
         abbr -a uvv 'uv venv --system-site-packages --no-managed-python'
     end
+
     if command -q chezmoi
-        abbr -a c chezmoi
+        abbr -a ch chezmoi
     end
 
     # abbr -a ;a '&'
@@ -30,4 +45,3 @@ end
 
 # 第二步：瞬间替换成正式的快捷方式
 # mv -Tf /opt/app/current.tmp /opt/app/current
-
